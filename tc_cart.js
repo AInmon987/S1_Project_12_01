0dd23ad0f8e3da1414503dbd2f7162bf42706da3
austin inmon <Ainmon987@west-mec.org>
2019-02-28 14:44:25 -0700

    Final commit
    Added the div with the id of cart on the html file. On the order javascript file Added the the array has an index of the id numbers of the items purchased, the array for the item description, item price, item Qty. Making all the strings show up in the html file made the javascript show up on the page. Added the total to where the value of the orderTotal value and the dollar sign will show up in front of the total. Added the value of orderTotal to the itemCost and to keep a running total of the total cost of the customer order. Added the value of the item cost is item price to multiply the item quantity. Made the item description show up for each item, the item will be the current items price from the price array, and the item quantity will be the current value from the quantity array. the for loop has the initial value of zero, there are 4 items and its adding by one.  Added the table for the item content. The initial value for the total of the order to zero. Ready for review

--- tc_cart.js
+++ tc_cart.js
@@ -11,3 +11,24 @@
    Filename: tc_cart.js
 	
 */
+// The inital value of the total of the order in dollars.
+var orderTotal = 0;
+// The var is setting the html content to show as a table.
+var cartHTML = "<table> <tr> <th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th> </tr>";
+// the for loop has the inital value of zero, there are 4 items and its adding by one. 
+for (var i = 0; i <= 3; i++) {
+    // setting the itme to the current item from the item array.
+    cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png'" + "alt=" + item[i] + "/></td>";
+    // making the item description show up for each item, the item will be the current items price from the price array, and the item quanity will be the current value from the quanity array.
+    cartHTML += "<td>" + itemDescription[i] + "</td>" + "<td>" + "$" + itemPrice[i] + "</td>" + "<td>" + itemQty[i] + "</td>";
+    //the value of the item cost is item price to multiply the item quanity. 
+    var itemCost = itemPrice[i] * itemQty[i];
+    //
+    cartHTML += "<td>" + '$' + itemCost + "</td></tr>";
+    //Adding the value of orderTotal to the itemCost and to keep a running total of thetotal cost of the customer order.
+    orderTotal += itemCost;
+}
+//the total is the value of the orderTotal value and the dollar sign will show up in front of the total.
+cartHTML += "<tr> <td colspan='4'>Subtotal</td>" + "<td>" + '$' + orderTotal + "</td>" + "</tr> </table>";
+// Making all the strings show up in the html file making the javascript show up on the page.
+document.getElementById("cart").innerHTML = cartHTML;
--- tc_order.js
+++ tc_order.js
@@ -16,3 +16,11 @@
    The itemQty array contains the quantity ordered of each item
    
 */
+//the array has an index of the id numbers of the items purchased.
+var item = [10582, 23015, 41807, 10041];
+//the array has the descriptions of the item with the first description starting at Zero. 
+var itemDescription = ["1975 Green Bay Packers Football(signed), Item 10582", "Tom Landry 1955 Football Card (unsigned), Item 23015", "1916 Army-Navy Game, Framed Photo (signed), Item 41807", "Protective Card Sheets, Item 10041"];
+// The array is names item price and has the price of the items.
+var itemPrice = [149.93, 89.98, 334.93, 22.67];
+//The array is name itemQTy and the index is the number of items per item. 
+var itemQty = [1, 1, 1, 4];
